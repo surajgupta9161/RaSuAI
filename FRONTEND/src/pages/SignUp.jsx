7ea35b09ba5f43acad912d05b656215b686de0f8
surajgupta <suraj666189@gmail.com>
2026-01-25 22:47:24 +0530

    taking name email and password by useState

--- FRONTEND/src/pages/SignUp.jsx
+++ FRONTEND/src/pages/SignUp.jsx
@@ -7,16 +7,27 @@ const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false)
     const navigate = useNavigate()
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function submitFunction(e) {
+        e.preventDefault()
+        console.log(name, "+", email, "+", password)
+
+    }
+
+
     return (
         <div className='w-full h-screen bg-cover flex justify-center items-center' style={{ backgroundImage: `url(${bg})` }}>
 
-            <form className='w-[90%] h-150 max-w-125 bg-[#00000070] backdrop-blur shadow-xl shadow-black rounded-2xl flex flex-col justify-center items-center gap-5 px-5'>
+            <form onSubmit={submitFunction} className='w-[90%] h-150 max-w-125 bg-[#00000070] backdrop-blur shadow-xl shadow-black rounded-2xl flex flex-col justify-center items-center gap-5 px-5'>
 
                 <h1 className='text-[25px] font-semibold mb-10'>Register to <span className='text-green-600 font-bold'>RaSu</span> your <span className='text-blue-800 font-bold'> Virtual Assistant </span> </h1>
-                <input type="text" placeholder='Enter your name' className='w-full h-15 outline-none border-2 bg-transparent rounded-full text-[18px] px-5 py-3 ' required />
-                <input type="email" placeholder='Enter your mail' className='w-full h-15 outline-none border-2 bg-transparent rounded-full text-[18px]  px-5 py-3 ' required />
+                <input type="text" placeholder='Enter your name' className='w-full h-15 outline-none border-2 bg-transparent rounded-full text-[18px] px-5 py-3 ' onChange={(e) => setName(e.target.value)} required />
+                <input type="email" placeholder='Enter your mail' className='w-full h-15 outline-none border-2 bg-transparent rounded-full text-[18px]  px-5 py-3 ' onChange={(e) => setEmail(e.target.value)} required />
                 <div className='w-full h-15 outline-none border-2 bg-transparent rounded-full text-[18px] relative'>
-                    <input type={showPassword ? "text" : "password"} placeholder='Enter your password' className='w-full h-full outline-none bg-transparent rounded-full text-[18px] px-5 py-3 ' required />
+                    <input type={showPassword ? "text" : "password"} placeholder='Enter your password' className='w-full h-full outline-none bg-transparent rounded-full text-[18px] px-5 py-3 ' onChange={(e) => setPassword(e.target.value)} required />
                     {!showPassword && <IoEyeOff className='absolute top-4.5 right-5 w-6.25 h-6.25 cursor-pointer' onClick={() => setShowPassword(true)} />}
                     {showPassword && <IoEye className='absolute top-4.5 right-5 w-6.25 h-6.25 cursor-pointer' onClick={() => setShowPassword(false)} />}
 
